a372847842c47b41e8bb17b4b7b347b5ad5defd7
d <d>
2020-12-13 12:41:10 +0800

    Init

---
+++ sdk.php
@@ -0,0 +1,5 @@
+<html><head></head><body><pre style="word-wrap: break-word; white-space: pre-wrap;">SDK Is not supported!
+Supported SDKs are:
+                   12.4
+                   13.1
+</pre></body></html>

5dd21a890c103f9d4eafa05149f944c53d3402e1
d <d>
2021-01-24 18:05:18 +0800

    Init

--- checkversion.js
+++
@@ -1,6 +0,0 @@
-var isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-if (isIOS) {
-console.log('This is a IOS device');
-} else {
-console.log('This is Not a IOS device');
-}
